5b8be1c289431a1cf1b41c8852c362145e293dbe
venu67776 <83317608+venu67776@users.noreply.github.com>
2025-08-05 13:09:11 +0530

    new

--- python-github/app.py
+++ python-github/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World from Flask running in Docker on port 5000 venu!"
+    return "Hello, World venu!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
